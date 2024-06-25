392570670fdb63266fe61dbc575555e11a2fa2bf
Struan Robertson <struanrobertson@pm.me>
2024-06-26 00:01:23 +0100

    Fix terminal emacsclient and set as EDITOR
    GUI emacsclient is set as VISUAL

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -9,7 +9,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
+   '("de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0"
+     "1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5"
      default))
  '(package-selected-packages nil)
  '(package-vc-selected-packages
