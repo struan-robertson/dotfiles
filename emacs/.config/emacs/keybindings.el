f9f4171911cb78f8212b20f295dc0e7203fc603e
Struan Robertson <struanrobertson@pm.me>
2024-06-27 12:52:40 +0100

    Keybindings for elisp evaluation

--- emacs/.config/emacs/keybindings.el
+++ emacs/.config/emacs/keybindings.el
@@ -1,1 +1,11 @@
- (global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; From https://www.masteringemacs.org/article/evaluating-elisp-emacs
+(defun mp-elisp-mode-eval-buffer ()
+  "Evaluate buffer and provide feedback that buffer has been evaluated"
+  (interactive)
+  (message "Evaluated buffer")
+  (eval-buffer))
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") #'mp-elisp-mode-eval-buffer)
+(define-key lisp-interaction-mode-map (kbd "C-c C-c") #'mp-elisp-mode-eval-buffer)
