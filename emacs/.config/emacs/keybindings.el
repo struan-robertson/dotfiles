e04e2907c0a27acb01585cbd48cc1d63892d3c6e
Struan Robertson <struanrobertson@pm.me>
2024-06-28 12:56:41 +0100

    Move keybindings for package to use-package :bind

--- emacs/.config/emacs/keybindings.el
+++ emacs/.config/emacs/keybindings.el
@@ -8,10 +8,3 @@
   (eval-buffer))
 (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'mp-elisp-mode-eval-buffer)
 (define-key lisp-interaction-mode-map (kbd "C-c C-c") #'mp-elisp-mode-eval-buffer)
-
-;; Multiple cursors on marked region
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-;; First mark word, then add more cursors
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
