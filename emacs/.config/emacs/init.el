8df076ab53c1ba26ffc6e0a4bb7fc71598df0fbd
Struan Robertson <struanrobertson@pm.me>
2024-06-28 20:04:47 +0100

    Eat nord colours

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -15,13 +15,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(auth-source-save-behavior nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eat-term-color-10 ((t (:inherit ansi-color-bright-black :foreground "#a3be8c"))))
+ '(eat-term-color-11 ((t (:inherit ansi-color-bright-black :foreground "#ebcb8b"))))
+ '(eat-term-color-12 ((t (:inherit ansi-color-bright-black :foreground "#5e81ac"))))
+ '(eat-term-color-2 ((t (:inherit ansi-color-green :foreground "#a3be8c"))))
+ '(eat-term-color-3 ((t (:inherit ansi-color-yellow :foreground "#ebcb8b"))))
+ '(eat-term-color-4 ((t (:inherit ansi-color-blue :foreground "#5e81ac"))))
+ '(eat-term-color-5 ((t (:inherit ansi-color-magenta :foreground "#b48ead"))))
+ '(eat-term-color-6 ((t (:inherit ansi-color-cyan :foreground "#88c0d0"))))
+ '(eat-term-color-8 ((t (:inherit nano-faded))))
  '(font-latex-bold-face ((t (:inherit bold))))
  '(font-latex-sectioning-5-face ((t (:inherit variable-pitch :foreground "#a3be8c" :weight bold))))
  '(font-latex-string-face ((t (:foreground "#d8dee9"))))
