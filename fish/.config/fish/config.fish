709e24faef2770f0f98a6066ff0388f039a1a787
Struan Robertson <struanrobertson@pm.me>
2024-07-13 15:12:03 +0100

    Add pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,13 @@
 # Set PATH
-set PATH ~/.local/share/juliaup/bin/ $PATH
+set PATH ~/.local/share/juliaup/bin/ ~/.local/bin $PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x BAT_THEME Nord
 
     zoxide init fish | source
+
+    pyenv init - | source
 
     alias vim='nvim'
 end
