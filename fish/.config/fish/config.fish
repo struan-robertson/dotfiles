7f5b486c3d74b481ad2638e713c25681c91797bf
Struan Robertson <struanrobertson@pm.me>
2024-07-16 21:46:11 +0100

    Add cargo to bin and remove pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,11 @@
 # Set PATH
-set PATH ~/.local/share/juliaup/bin/ ~/.local/bin $PATH
+set PATH ~/.local/share/juliaup/bin/ ~/.local/bin ~/.cargo/bin/ $PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x BAT_THEME Nord
 
     zoxide init fish | source
-
-    pyenv init - | source
 
     alias vim='nvim'
 end
