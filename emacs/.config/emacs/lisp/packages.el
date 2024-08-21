072e3d05b6e362e9ee3bb2be73f178e3d964da2b
Struan Robertson <struanrobertson@pm.me>
2024-08-21 15:50:18 +0100

    .

--- emacs/.config/emacs/lisp/packages.el
+++ emacs/.config/emacs/lisp/packages.el
@@ -694,7 +694,7 @@ FN is `eglot--executable-find', ARGS is the arguments to `eglot--executable-find
 (use-package python
   :config
   (setq python-shell-interpreter "ipython"
-        python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True --profile=emacs")
+	python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True --profile=emacs")
   (indent-tabs-mode nil))
 
 
@@ -776,21 +776,21 @@ FN is `eglot--executable-find', ARGS is the arguments to `eglot--executable-find
   (defun my/vc-git-state (file)
     "`vc-state' which does not include ignored files."
     (let* ((args
-            `("status" "--porcelain" "-z"
-              ;; Just to be explicit, it's the default anyway.
-              "--untracked-files"
-              "--"))
-           (status (apply #'vc-git--run-command-string file args)))
+	    `("status" "--porcelain" "-z"
+	      ;; Just to be explicit, it's the default anyway.
+	      "--untracked-files"
+	      "--"))
+	   (status (apply #'vc-git--run-command-string file args)))
       (if (null status)
-          ;; If status is nil, there was an error calling git, likely because
-          ;; the file is not in a git repo.
-          'unregistered
+	  ;; If status is nil, there was an error calling git, likely because
+	  ;; the file is not in a git repo.
+	  'unregistered
 	;; If this code is adapted to parse 'git status' for a directory,
 	;; note that a renamed file takes up two null values and needs to be
 	;; treated slightly more carefully.
 	(vc-git--git-status-to-vc-state
 	 (mapcar (lambda (s)
-                   (substring s 0 2))
+		   (substring s 0 2))
 		 (split-string status "\0" t))))))
   
   
