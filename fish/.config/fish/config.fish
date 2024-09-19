1f494236b1bc0e8a169eda8930904dbeee56d217
Struan Robertson <struanrobertson@pm.me>
2024-09-19 22:53:52 +0100

    Fix fish prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,25 @@
 set PATH ~/.local/share/juliaup/bin/ ~/.local/bin ~/.cargo/bin/ $PATH
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    
     set -x BAT_THEME Nord
 
     zoxide init fish | source
 
+    # Improve Emacs keybindings
+    bind \ep up-or-search
+    bind \en down-or-search
+    bind \er history-pager
+
 end
 
+# Allow Emacs TRAMP Connections
+if test "$TERM" = "dumb"
+  function fish_prompt
+    echo "\$ "
+  end
+
+  function fish_right_prompt; end
+  function fish_greeting; end
+  function fish_title; end
+end
