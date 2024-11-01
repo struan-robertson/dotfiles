cf5edf3de2a53ccb41617d6ef55d6cc51c184cad
Struan Robertson <struanrobertson@pm.me>
2024-11-01 14:01:33 +0000

    Remove nyxt

--- nyxt/.config/nyxt/dark-reader.lisp
+++
@@ -1,9 +0,0 @@
-;;; ~/.config/nyxt/dark-reader.lisp
-
-(define-configuration nx-dark-reader:dark-reader-mode
-  ((nxdr:selection-color "#5E81AC")
-   (nxdr:background-color "#2E3440")
-   (nxdr:text-color "#ECEFF4")))
-
-(define-configuration web-buffer
-  ((default-modes `(nxdr:dark-reader-mode ,@%slot-value%))))
