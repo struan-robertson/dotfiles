992b52c9384945768430b0b09bade1d4e638013e
Struan Robertson <struanrobertson@pm.me>
2025-01-14 21:48:16 +0000

    Better python.el bindings

--- emacs/.config/emacs/lisp/packages.el
+++ emacs/.config/emacs/lisp/packages.el
@@ -842,7 +842,10 @@ If so, return path to .venv/bin"
     (let ((python-shell-virtualenv-root (my/detect-venv default-directory)))
       (apply fn args)))
   
-  (advice-add 'run-python :around #'my/python-wrap-venv-advice))
+  (advice-add 'run-python :around #'my/python-wrap-venv-advice)
+  :bind (:map python-mode-map
+	      ("C-c C-c" . python-shell-send-statement)
+	      ("C-c C-b" . python-shell-send-buffer)))
 
 
 ;;;;; eshell-venv
