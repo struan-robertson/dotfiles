262728733ae10f65c65828446ab7102a660b90a7
Struan Robertson <contact@struan.tech>
2025-04-21 19:48:16 +0100

    Add consult-dir package

--- emacs/.config/emacs/lisp/packages.el
+++ emacs/.config/emacs/lisp/packages.el
@@ -812,6 +812,35 @@ If so, return path to .venv/bin"
   :bind
   ("M-g t" . consult-todo))
 
+;;;;; consult-dir
+;; Allow inserting recently visited directories into the minibuffer
+(use-package consult-dir
+  :ensure t
+  :bind (("C-x C-d" . consult-dir)
+	 :map vertico-map
+	 ("C-x C-d" . consult-dir)
+	 ("C-x C-j" . consult-dir-jump-file))
+  :config
+  (defun eshell/z (&optional regexp)
+    "Navigate to a previously visited directory in eshell, or to
+any directory proferred by `consult-dir'."
+    (let ((eshell-dirs (delete-dups
+                        (mapcar 'abbreviate-file-name
+                                (ring-elements eshell-last-dir-ring)))))
+      (cond
+       ((and (not regexp) (featurep 'consult-dir))
+        (let* ((consult-dir--source-eshell `(:name "Eshell"
+						   :narrow ?e
+						   :category file
+						   :face consult-file
+						   :items ,eshell-dirs))
+               (consult-dir-sources (cons consult-dir--source-eshell
+                                          consult-dir-sources)))
+          (eshell/cd (substring-no-properties
+                      (consult-dir--pick "Switch directory: ")))))
+       (t (eshell/cd (if regexp (eshell-find-previous-directory regexp)
+                       (completing-read "cd: " eshell-dirs))))))))
+
 ;;;;; wgrep
 ;; Allow editing grep buffers
 (use-package wgrep)
