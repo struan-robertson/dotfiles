f95c47afad765e76f390606b4bd252c763f5fe7d
Struan Robertson <contact@struan.tech>
2025-09-09 14:06:01 +0100

    init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -62,7 +62,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
- '(org-ics-import-confirmed-overwrite '("~/Sync/Notes/Tasks/ical/university.org"))
+ '(org-ics-import-confirmed-overwrite '("~/Documents/Notes/Tasks/ical/university.org"))
  '(pdf-annot-default-annotation-properties
    '((t (label . "Struan Robertson"))
      (text (color . "#bf616A") (icon . "Note"))
