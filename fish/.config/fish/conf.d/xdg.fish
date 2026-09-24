9d118e7a9a1680135f00fd5a36ad7ff6c53f486f
Struan Robertson <contact@struan.tech>
2026-09-24 13:06:25 +0100

    Home dotfile cleanup

--- fish/.config/fish/conf.d/xdg.fish
+++ fish/.config/fish/conf.d/xdg.fish
@@ -5,7 +5,6 @@ set -q XDG_STATE_HOME;  or set -gx XDG_STATE_HOME  $HOME/.local/state
 set -q XDG_CACHE_HOME;  or set -gx XDG_CACHE_HOME  $HOME/.cache
 
 set -gx CLAUDE_CONFIG_DIR $XDG_CONFIG_HOME/claude
-set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
 set -gx KODI_DATA $XDG_DATA_HOME/kodi
@@ -15,7 +14,6 @@ set -gx WINEPREFIX $XDG_DATA_HOME/wine
 set -gx NODE_REPL_HISTORY $XDG_STATE_HOME/node_repl_history
 set -gx NPM_CONFIG_INIT_MODULE $XDG_CONFIG_HOME/npm/config/npm-init.js
 set -gx NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
-set -gx NPM_CONFIG_TMP $XDG_RUNTIME_DIR/npm
 set -gx PYTHON_HISTORY $XDG_STATE_HOME/python_history
 
 set -gx _JAVA_OPTIONS -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
